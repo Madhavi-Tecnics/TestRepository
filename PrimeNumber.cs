3e62bbb446745f3669e03e06c9cebaa3f0684240
Madhavi-Tecnics <141643460+Madhavi-Tecnics@users.noreply.github.com>
2023-08-14 12:37:21 +0530

    Create PrimeNumber.cs

---
+++ PrimeNumber.cs
@@ -0,0 +1,28 @@
+using System;  
+  public class PrimeNumberExample  
+   {  
+     public static void Main(string[] args)  
+      {  
+          int n, i, m=0, flag=0;    
+          Console.Write("Enter the Number to check Prime: ");    
+          n = int.Parse(Console.ReadLine());  
+          m=n/2;    
+          for(i = 2; i <= m; i++)    
+          {    
+           if(n % i == 0)    
+            {    
+             Console.Write("Number is not Prime.");    
+             flag=1;    
+             break;    
+            }    
+          }    
+          if (flag==0)    
+           Console.Write("Number is Prime.");       
+     }  
+   }  
+Output:
+
+Enter the Number to check Prime: 17  
+Number is Prime.   
+Enter the Number to check Prime: 57  
+Number is not Prime.
